444d314edad587560f2f60f7101f6890ce412b9d
thockyantho <anthopham94@gmail.com>
2026-07-23 19:55:06 -0700

    Step 3: Created an entry point

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => {
-  console.log("listening on port ${PORT}");
+  console.log(`listening on port ${PORT}`);
 });
