c44d624f668534ee27a53ef83505a594df23008f
thockyantho <anthopham94@gmail.com>
2026-07-23 20:12:38 -0700

    Step 6. Created and connceted a database

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
