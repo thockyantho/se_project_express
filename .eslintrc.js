b5d1eada6e51605a46fb361b42e018b56e620d78
thockyantho <anthopham94@gmail.com>
2026-07-23 19:32:53 -0700

    Add linter exception to underscore

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: { "no-console": ["warn", { allow: ["error"] }] },
+  rules: {
+    "no-console": ["warn", { allow: ["error"] }],
+    "no-underscore-dangle": "off",
+  },
 };
