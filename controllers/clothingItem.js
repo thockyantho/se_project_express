e7ceab0a77f2eff006b82442b3e2cf8e706e184b
thockyantho <anthopham94@gmail.com>
2026-08-20 22:42:04 -0700

    ClothingItem controllers @18:01

---
+++ controllers/clothingItem.js
@@ -0,0 +1,18 @@
+const ClothingItem = require("../models/clothingItem.js");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+
+  const { name, weather, imageUrl } = req.body;
+
+  ClothingItem.create({ name, weather, imageUrl })
+    .then((item) => res.status(201).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
