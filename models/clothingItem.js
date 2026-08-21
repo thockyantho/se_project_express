af0812dd72c3fd23ac567df3edb7707e8787f956
thockyantho <anthopham94@gmail.com>
2026-08-20 22:31:00 -0700

    clothingItem.js video Part 9

---
+++ models/clothingItem.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],},
+    weather: {
+    type: String,
+    required: true,}
+    imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Link is not Valid",
+    },
+  },
+});
+
+module.exports = mongoose.model("clothingItems", clothingItem);
--- models/clothingItems.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const clothingItemsSchema = new mongoose.Schema({});
-
-module.exports = mongoose.model("item", clothingItemsSchema);
