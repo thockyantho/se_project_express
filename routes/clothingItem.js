a8aca8cad738228514f08d3c02879e271a633984
thockyantho <anthopham94@gmail.com>
2026-09-10 23:04:20 -0700

    added delete router

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -4,6 +4,7 @@ const {
   createItem,
   getItems,
   updateItem,
+  deleteItem,
 } = require("../controllers/clothingItem.js");
 
 //CRUD
@@ -18,5 +19,6 @@ router.get("/", getItems);
 router.put("/:itemId", updateItem);
 
 //Delete
+router.delete("/:itemId", deleteItem);
 
 module.exports = router;
