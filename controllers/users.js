64801a4bc7e22e45487888e8900559af1c2566e0
thockyantho <anthopham94@gmail.com>
2026-09-21 18:42:12 -0700

    adjusted if statement

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,8 @@ const getUser = (req, res) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: "User not found" });
-      } else if (err.name === "CastError") {
+      }
+      if (err.name === "CastError") {
         return res.status(400).send({ message: "Invalid user ID" });
       }
       return res.status(500).send({ message: err.message });
