a459cfc4bb23b0ecba1feef216941ca02d503f5e
thockyantho <anthopham94@gmail.com>
2026-09-21 18:04:17 -0700

    adjusted clothingItems to clothingItem

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -40,4 +40,4 @@ const clothingItem = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("clothingItems", clothingItem);
+module.exports = mongoose.model("clothingItem", clothingItem);
