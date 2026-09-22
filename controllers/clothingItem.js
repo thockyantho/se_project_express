2ce06201858900e870569bf8781cd10e45248e88
thockyantho <anthopham94@gmail.com>
2026-09-21 19:47:20 -0700

    adjusted status messages

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -11,9 +11,9 @@ const createItem = (req, res) => {
       if (err.name === "ValidationError") {
         return res.status(BAD_REQUEST).send({ message: err.message });
       }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "Error from createItem" });
+      return res.status(SERVER_ERROR).send({
+        message: "An error has occurred on the server.",
+      });
     });
 };
 
@@ -22,7 +22,9 @@ const getItems = (req, res) => {
     .then((items) => res.status(200).send({ data: items }))
     .catch((err) => {
       console.error(err);
-      return res.status(SERVER_ERROR).send({ message: "Error from getItems" });
+      return res.status(SERVER_ERROR).send({
+        message: "An error has occurred on the server.",
+      });
     });
 };
 
@@ -30,12 +32,31 @@ const updateItem = (req, res) => {
   const { itemId } = req.params;
   const { imageUrl } = req.body;
 
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+  ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $set: { imageUrl } },
+    { new: true, runValidators: true }
+  )
     .orFail()
     .then((item) => res.status(200).send({ data: item }))
-    .catch((e) =>
-      res.status(SERVER_ERROR).send({ message: "Error from updateItem", e })
-    );
+    .catch((err) => {
+      console.error(err);
+
+      if (err.name === "ValidationError" || err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({
+          message: "Invalid data",
+        });
+      }
+
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({
+          message: "Item not found",
+        });
+      }
+      return res.status(SERVER_ERROR).send({
+        message: "An error has occurred on the server.",
+      });
+    });
 };
 
 const deleteItem = (req, res) => {
