54ad0382f68bff00bf011695bd66e4be7c4a8b9b
thockyantho <anthopham94@gmail.com>
2026-09-21 17:50:33 -0700

    added character requirements to schema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -5,6 +5,8 @@ const clothingItem = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Name is required"],
+    minlength: [2, "The minimum length is 2"],
+    maxlength: [30, "The maximum length is 30"],
   },
   weather: {
     type: String,
