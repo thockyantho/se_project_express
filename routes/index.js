245a0a02ba103e6082b34033498be2c001c4fa6e
thockyantho <anthopham94@gmail.com>
2026-09-22 10:12:02 -0700

    Imported NOT_FOUND

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItem");
+const { NOT_FOUND } = require("../utils/errors");
 
 const userRouter = require("./users");
 
